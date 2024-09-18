ad1c9b617b4f5fe8192f42b155f514bc92730288
Fernanda Andreia Nascimento <47371224+fernandaandreia@users.noreply.github.com>
2024-09-18 11:24:30 -0300

    Initial Commit

---
+++ MeetingSync/MeetingSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingSyncApp.swift
+//  MeetingSync
+//
+//  Created by Fernanda on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MeetingSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
